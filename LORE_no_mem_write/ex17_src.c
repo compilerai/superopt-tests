e654d76a00c1077709a8f8d25a0749ffa574f5d9
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2023-06-22 23:38:59 +0530

    LORE_mem_write, LORE_no_mem_write: inline #include "globals.h" and #include "globals_srcdst.h" to make the C sources freestanding
    otherwise eqcheck fails due to compilation error

--- LORE_no_mem_write/ex17_src.c
+++ LORE_no_mem_write/ex17_src.c
@@ -1,4 +1,10 @@
-#include "globals_srcdst.h"
+#define LEN 32000
+#define LEN1 3200
+#define LEN2 2048
+
+#define TYPE int
+__attribute__((aligned(16))) TYPE a[LEN],b[LEN],c[LEN],d[LEN],e[LEN],
+                                   aa[LEN2][LEN2],bb[LEN2][LEN2],cc[LEN2][LEN2],tt[LEN2][LEN2];
 
 //remainder loop  fusion
 //Sum
