1f00b90562a05b08e3f3fda2d728ffc229cd4508
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2023-07-18 19:25:57 +0530

    localmem-tests: local_used_in_unreachable_loop.c: added local_used_in_unreachable_loop_vla()

--- localmem-tests/local_used_in_unreachable_loop.c
+++ localmem-tests/local_used_in_unreachable_loop.c
@@ -1,10 +1,21 @@
 void MYmyfoo(int*);
+void MYmybar(int*,int);
 
 int local_used_in_unreachable_loop(void)
 {
   int ret = 0;
   while (ret) {
     MYmyfoo(&ret);
+  }
+  return ret;
+}
+
+int local_used_in_unreachable_loop_vla(void)
+{
+  int ret = 0;
+  while (ret) {
+    int v[ret+1];
+    MYmybar(v, ret+1);
   }
   return ret;
 }
