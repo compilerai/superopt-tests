f9d729451889029f80655289d04a5fa0a33334a6
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2023-07-26 20:02:15 +0530

    micro: updated Makefile and slightly tweaked benchmarks to rule out IPO, vectorization

--- micro/sum_positive.c
+++ micro/sum_positive.c
@@ -5,12 +5,15 @@ int g[1000];
 int sum_positive_global(int n)
 {
   int ret = 0;
+#pragma clang loop vectorize(disable) unroll(disable)
   for (int i = 0; i < n; i++)
     if (g[i] > 0)
       ret += g[i];
   return ret;
 }
 
+/*
+// requires ptr arith UB modeling
 int sum_positive_arg(int* arr, int n)
 {
   int ret = 0;
@@ -19,3 +22,4 @@ int sum_positive_arg(int* arr, int n)
       ret += arr[i];
   return ret;
 }
+*/
