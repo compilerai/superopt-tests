d2953703da812de69058d0544a7e80321e1ce4c1
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2024-01-05 20:15:20 +0530

    demo: added fib

--- demo/sample.c
+++ demo/sample.c
@@ -1,5 +1,20 @@
-int sample()
+void MYmyprintf(const char* fmt,...);
+
+int fib(int n, int m)
 {
-  // YOUR CODE HERE
-  return 0;
+  int v[n+2];
+  v[0] = 0;
+  v[1] = 1;
+  for (int i = 2; i <= m; ++i) {
+    v[i] = v[i-1]+v[i-2];
+  }
+  MYmyprintf("fib(%d) = %d", m, v[m]);
+  return v[m];
 }
+
+// Uncomment the following for adding your own code
+// int sample()
+// {
+//   // YOUR CODE HERE
+//   return 0;
+// }
