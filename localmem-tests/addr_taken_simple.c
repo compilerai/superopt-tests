2de24582c5ab59149fb2747f89b94beccea9a986
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2024-03-15 18:15:29 +0530

    localmem-tests: added addr_taken_structs

--- localmem-tests/addr_taken_simple.c
+++ localmem-tests/addr_taken_simple.c
@@ -1,8 +1,21 @@
 void foo(int*);
 
-int addr_taken_simple(void)
+int addr_taken_scalar(void)
 {
   int ret;
   foo(&ret);
   return ret;
+}
+
+struct Point {
+  int x; int y;
+};
+
+void read_points(struct Point* p1, struct Point* p2);
+
+int addr_taken_structs(void)
+{
+  struct Point p1, p2;
+  read_points(&p1, &p2);
+  return p1.x - p2.x;
 }
