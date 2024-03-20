f94c91c866b2ff545831b0632a42aa642d57d2d7
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2024-03-20 11:56:00 +0530

    localmem-tests/addr_taken_parameter.c: added a large struct testcase

--- localmem-tests/addr_taken_parameter.c
+++ localmem-tests/addr_taken_parameter.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int addr_taken_parameter_scalar(int a)
 {
@@ -15,7 +16,7 @@ struct Point {
   int x; int y;
 };
 
-int addr_taken_parameter_structs(struct Point p1, struct Point p2)
+int addr_taken_parameter_small_struct(struct Point p1, struct Point p2)
 {
   char x;
   if (p1.x == 0 || p2.x == 0) {
@@ -24,4 +25,19 @@ int addr_taken_parameter_structs(struct Point p1, struct Point p2)
     scanf("%c", &x);
   }
   return x+(p1.x+p1.y)+(p2.x+p2.y);
+}
+
+struct Str {
+  short is_small;
+  char buf[80];
+  char *ptr;
+};
+
+
+int addr_taken_parameter_large_struct(struct Str s)
+{
+  scanf("%hd", &s.is_small);
+  char* p = s.is_small ? s.buf : s.ptr;
+  scanf("%s",p);
+  return strlen(p);
 }
