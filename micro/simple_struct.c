55dbe31a45cd67367e12c15275a99426494c35cd
Gokul Sankar <csz238080@cse.iitd.ac.in>
2024-05-24 10:11:06 +0530

    Micro: Added test for indirect call returning struct and updated expected_fails

--- micro/simple_struct.c
+++ micro/simple_struct.c
@@ -8,9 +8,17 @@ struct foo ret_struct(int n, int m)
 	struct foo t = {n, m};
 	return t;
 }
- 
+
 int call_ret_struct(int n, int m)
 {
 	struct foo t1 = ret_struct(n, m);
+	return t1.a;
+}
+
+typedef struct foo (*func_t)(int, int);
+
+int call_ret_struct_indir(func_t struct_fcall, int n, int m)
+{
+	struct foo t1 = struct_fcall(n, m);
 	return t1.a;
 }
