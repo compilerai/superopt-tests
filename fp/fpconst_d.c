e40ccfcbc97d125f15b36bf2b2a9b3ffc9e3d2e9
Sorav Bansal <sbansal@iitd.ac.in>
2024-05-26 15:12:57 +0530

    renamed functions in fp/fpconst_d.c

--- fp/fpconst_d.c
+++ fp/fpconst_d.c
@@ -1,44 +1,43 @@
-double add_const(double a)
+double add_const_d(double a)
 {
   return a + 3.14159;
 }
 
-double mult_const(double a)
+double mult_const_d(double a)
 {
   return a * 3.14159;
 }
 
-int cmp_const(double a)
+int cmp_const_d(double a)
 {
   return a == 3.14159 || 2*a > 1.41;
 }
 
-double expr1()
+double expr1_d()
 {
   return 1.0/3;
 }
 
-double expr2(double a)
+double expr2_d(double a)
 {
   double b = 0.12345;
 
   return a/b + (a*a)/(b*b) + (a*a*a)/(b*b*b);
 }
 
-double expr3(double a)
+double expr3_d(double a)
 {
   return (3.1*a*a + 2.5*a + 7.9) / (0.9*a*a + 6.5*a + 8.4);
 }
 
-double expr3_1(double a)
+double expr3_1_d(double a)
 {
   return (3.1*a*a + 7.9) / (0.9*a*a + 8.4);
 }
 
-double const_expr()
+double const_expr_d()
 {
   double f = 0.316438636889084;
 
   return f + f*f + 1/f + 1/(f*f);
 }
-
