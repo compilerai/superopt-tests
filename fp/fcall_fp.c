8bc72e15d2a9aa7d07b771f99e5aad4ddde894ac
Sorav Bansal <sbansal@iitd.ac.in>
2024-06-09 22:42:01 +0530

    Added more functions to fp/fcall_fp.c

--- fp/fcall_fp.c
+++ fp/fcall_fp.c
@@ -4,3 +4,17 @@ int fcall_fp(int blockSize)
 {
   return (int)pow(2, blockSize+1)-1;
 }
+
+int fcall_fp2(int blockSize)
+{
+  int i1 = (int)pow(2, blockSize+1)-1;
+
+  return (int)pow(2, blockSize+1)-i1;
+}
+
+int fcall_fp2a(int blockSize)
+{
+  int i1 = (int)pow(2, blockSize+1)-1;
+
+  return (int)pow(i1, blockSize+1)-1;
+}
