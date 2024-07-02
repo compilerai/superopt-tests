746ca025409fff07cde89e1d1df9f80979679e56
8have5h <cs5210594@iitd.ac.in>
2024-07-02 13:24:55 +0530

    test_cases

---
+++ misra-c-rules/R_5_4.c
@@ -0,0 +1,8 @@
+#define f(x, y) (x) + (y)
+#define f 5
+#define g(x, y) (x) - (y)
+#define t(x, y) x * y
+
+int main(){
+    return 0;
+}
--- misra-c-rules/R_7_4.c
+++ misra-c-rules/R_7_4.c
@@ -1,11 +1,29 @@
 // MISRA C R 7.4 :: A string literal shall not be assigned to an object unless the object's type is "pointer to const-qualified char"
+// void test1(){
+//     const char *greeting = "Good morning!"; 
+//     char editableGreeting[] = "Good morning!"; 
+//     editableGreeting[5] = 'a';
+// }
+// int main () {
+//     const char *message1 = "Hello, World!";  // Correct: pointer to const-qualified char
+//     char *message2 = "Hello, World!";  // Incorrect: pointer to non-const char
+//     return 0;
+// }
 void test1(){
-    const char *greeting = "Good morning!"; 
-    char editableGreeting[] = "Good morning!"; 
-    editableGreeting[5] = 'a';
+    const char *x = "hi";
 }
+void test2(){
+    char* x = "hello"; // would raise !!
+}
+void test3(){
+    char *message1 = "Hello, World!";    // would raise.
+    char *m2 = message1; // wont.
+}
+void test4(){
+    char xy[] = "Hi";
+    char *z = xy;
+}
+
 int main () {
-    const char *message1 = "Hello, World!";  // Correct: pointer to const-qualified char
-    char *message2 = "Hello, World!";  // Incorrect: pointer to non-const char
     return 0;
 }
