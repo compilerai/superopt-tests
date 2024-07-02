4f9b50c8dba8bc69123b94880d5d886a44939d13
8have5h <cs5210594@iitd.ac.in>
2024-07-02 17:12:14 +0530

    Updated Test Case

--- misra-c-rules/R_7_4.c
+++ misra-c-rules/R_7_4.c
@@ -1,20 +1,26 @@
+// correct.
 void test1(){
     const char *x = "hi";
 }
+// incorrect.
 void test2(){
     char* x = "hello"; // would raise !!
 }
+// both .
 void test3(){
     char *message1 = "Hello, World!";    // would raise.
     char *m2 = message1; // wont.
 }
+// array.
 void test4(){
     char xy[] = "Hi";
     char *z = xy;
 }
-
+//
 int main () {
     test1();
     test2();
+    test3();
+    test4();
     return 0;
 }
