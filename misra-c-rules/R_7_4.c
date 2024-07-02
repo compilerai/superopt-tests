0e769bb3cc25b261ddc5b463759b12a289710727
8have5h <cs5210594@iitd.ac.in>
2024-07-02 13:33:35 +0530

    Test Case Update

--- misra-c-rules/R_7_4.c
+++ misra-c-rules/R_7_4.c
@@ -1,14 +1,3 @@
-// MISRA C R 7.4 :: A string literal shall not be assigned to an object unless the object's type is "pointer to const-qualified char"
-// void test1(){
-//     const char *greeting = "Good morning!"; 
-//     char editableGreeting[] = "Good morning!"; 
-//     editableGreeting[5] = 'a';
-// }
-// int main () {
-//     const char *message1 = "Hello, World!";  // Correct: pointer to const-qualified char
-//     char *message2 = "Hello, World!";  // Incorrect: pointer to non-const char
-//     return 0;
-// }
 void test1(){
     const char *x = "hi";
 }
@@ -25,5 +14,7 @@ void test4(){
 }
 
 int main () {
+    test1();
+    test2();
     return 0;
 }
