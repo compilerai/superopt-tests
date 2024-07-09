373853c5176435e7c3803b5a715a17577c939171
8have5h <cs5210594@iitd.ac.in>
2024-07-09 12:42:17 +0530

    Test cases additions

--- misra-c-rules/R_8_10.c
+++ misra-c-rules/R_8_10.c
@@ -14,9 +14,14 @@ shall raise warnings
 inline int sub(int a, int b) {
     return a - b;
 }
+// check what happens , when u remove static from one of them.
+static inline int func(int , int);
 int main() {
     int x = 5, y = 3;
     int result = add(x, y);
     printf("Result: %d\n", result);
     return 0;
+}
+static inline int func(int a, int b) {
+    return (a + b);
 }
