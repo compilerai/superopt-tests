15d61ddd52e57f9c72234df72c40a14c3fda71a6
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2024-08-21 20:10:59 +0530

    localmem-tests/fib.c: disable vectorization

--- localmem-tests/fib.c
+++ localmem-tests/fib.c
@@ -5,6 +5,7 @@ int fib(int n, int m)
   int v[n+2];
   v[0] = 0;
   v[1] = 1;
+#pragma clang loop vectorize(disable) unroll(disable)
   for (int i = 2; i <= m; ++i) {
     v[i] = v[i-1]+v[i-2];
   }
