a498874e039b610bc58e23c453c6f699e2ff9cdc
PiyushC1480 <cs1211010@iitd.ac.in>
2024-11-04 05:27:21 +0530

    Testcases added for R_7_2

--- misra-c-rules/R_7_2.c
+++ misra-c-rules/R_7_2.c
@@ -1,11 +1,19 @@
 /*
-    The lowercase character 'l' shall not be used in a literal suffix
+    A "u" or "U" suffix shall be applied to all integer constants that are represented in an unsigned type
 */
 
 #include <stdio.h>
 
+#include <stdio.h>
 int main(){
-    // use of L should not raise a warning
-    long x = 1L; // this is a literal suffix
+    unsigned int num1 = 4532455u;
+    
+    unsigned int num2 = 4532455U;
+    const unsigned int num3 = 1384849u;
+    const unsigned int num4 = 89898;
+    const unsigned num5 = 989882U;
+    int num6 = 4345345;  //decalring int with u at the end
+
     return 0;
+
 }
