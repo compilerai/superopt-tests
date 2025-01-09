cac53da4d4a6c8537278a589b93d57a863bc4c52
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2025-01-09 12:01:27 +0530

    micro: added unreachable_alloc_in_src_pathset_with_two_allocs()
    we currently fail to prove it

--- micro/unreachable_alloc_in_src_pathset.c
+++ micro/unreachable_alloc_in_src_pathset.c
@@ -1,15 +1,38 @@
 #include <alloca.h>
 
-// This function triggers the unreachable alloc-dealloc in src path wfcond for
-// a correctly constructed CG
-// The wfcond has to be checked for the entry to exit correlation where the src
-// pathset includes paths to both returns (due to being full-pathset), but the
-// dst only includes the early exit path (because the loop anchor node lies in
-// the way to the exit node and prohibits its enumeration)
+// The eqproof of this function requires enumeration of a pathset without alloc edges.
+// For the early exit path in dst, our full-pathset enumeration would
+// enumerate pathset containing both the early exit path and the path through
+// the loop, call the latter "second path".  The second path contains an alloc.
+// Our alloc/dealloc correlation algorithm requires syntactic matching of
+// alloc/dealloc in correlated pathsets. Because the early exit path of dst
+// does not contain an alloc (required for correlating the alloc in second path),
+// this correlation is rejected.
+// We enumerate an additional pathset without the alloc that correlates successfully.
 int unreachable_alloc_in_src_pathset(int n)
 {
   if (n < 1) {
     return 0;
+  }
+  int p[n];
+  // compiler removes the (always true) initial condition check of this loop
+#pragma clang loop vectorize(disable) unroll(disable)
+  for (int i = 0; i < n; ++i) {
+    p[i] = i;
+  }
+  return p[0];
+}
+
+// the following function builds upon the previous function and
+// adds an alloca() in the early exit path.
+// This defeats our "enumerate additional pathset without alloc edges"
+// strategy.
+// Note that such a code is highly unlikely to appear in practice.
+int unreachable_alloc_in_src_pathset_with_two_allocs(int n)
+{
+  if (n < 1) {
+    int* a = alloca(sizeof(int));
+    return (int)a;
   }
   int p[n];
   // compiler removes the (always true) initial condition check of this loop
